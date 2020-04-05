50529de1dcb5a8a34365c00cf711d12faab033eb
PIXEL-SHK <54989258+PIXEL-SHK@users.noreply.github.com>
2020-04-05 17:13:37 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,13 +2,20 @@ import discord
 import random
 import os
 from discord.ext import commands
+import datetime
 
 client = commands.Bot(command_prefix = '$')
 
 @client.event
 async def on_ready():
     print('Bot is ready')
-    
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("Chat Moritoring")
+    await client.change_presence(status=discord.Status.online, activity=game)
+ 
 @client.command()
 async def 청소(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
@@ -20,6 +27,7 @@ async def kick(ctx, member : discord.Member, *, reason=None):
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
+
 
 access_token = os.environ["BOT_TOKEN"]  
 client.run(access_token)
